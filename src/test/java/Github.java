946140da09efe084f1e2d671da90de2c08ff56f6
mirza <mihracg@yahoo.com>
2023-08-20 07:47:13 -0700

    latest_commit

--- src/test/java/Github.java
+++ src/test/java/Github.java
@@ -1,5 +1,6 @@
 public class Github {
     public static void main(String[] args) {
-        System.out.println("Hello Githubsammms");
+        System.out.println("Hello Github");
+        //mork work added here
     }
 }
